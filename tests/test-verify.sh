6bcdcd142932c35f173151c942cacc8f9f3f22fc
Adam Dyson <adam.dyson@outlook.com>
2018-05-22 18:49:22 +1000

    Added Ansible 2.4 support

--- tests/test-verify.sh
+++ tests/test-verify.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+# Copyright (c) Ontic. (http://www.ontic.com.au). All rights reserved.
+# See the COPYING file bundled with this package for license details.
 
-# Verify the user and group exists.
+# Verify the group exists.
 docker exec --tty ${container_id} env TERM=xterm awk -v val=1333 -F ":" '$3==val{print "Group: "$1" (1333)"}' /etc/group
+# Verify the user exists.
 docker exec --tty ${container_id} env TERM=xterm awk -v val=1444 -F ":" '$3==val{print "User: "$1" (1444)"}' /etc/passwd
